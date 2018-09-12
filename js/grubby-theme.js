2927e80109cbf0c272895353fa059442c10d24ce
disjfa <disjfa@disjfa.nl>
2018-09-12 21:54:47 +0000

    first commit

---
+++ js/grubby-theme.js
@@ -0,0 +1,8 @@
+require('./../scss/grubby-theme.scss');
+
+var Popper = require('popper.js');
+window.Popper = Popper;
+
+require('bootstrap/dist/js/bootstrap.js');
+require('chart.js');
+require('./dashboard');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+  .setOutputPath('site/build/')
+  .setPublicPath('/grubby-theme/build')
+  .addEntry('grubby-theme', './js/grubby-theme.js')
+  .setManifestKeyPrefix('build/')
+  .enableSassLoader()
+  .autoProvidejQuery()
+  .enablePostCssLoader()
+  .enableSourceMaps(!Encore.isProduction())
+;
+if (Encore.isProduction()) {
+  Encore
+    .cleanupOutputBeforeBuild()
+  ;
+}
+
+module.exports = Encore.getWebpackConfig();
